8ef28903f8906c23328364ec9783a3bb7be119dc
Michael Peacock <mkpeacock@gmail.com>
2019-02-07 12:32:12 +0000

    Add test to assert that GET /api/1.0/events returns 200

---
+++ tests/Feature/Api/OnePointZero/EventsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Api\OnePointZero;
+
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class EventsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp()
+    {
+        $this->faker = Faker::create();
+
+        parent::setUp();
+    }
+
+    /** @test */
+    public function get_request_on_events_endpoint_returns_200_status_code(): void
+    {
+        $response = $this->json('GET', '/api/1.0/events');
+        $response->assertStatus(200);
+    }
+}
